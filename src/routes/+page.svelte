6557fd7a44c0df11b03f02b4e5744f4e2ac1288b
zalwan <rizalsuryawan86@gmail.com>
2025-11-24 22:40:28 +0700

    feat: update sample layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="flex min-h-screen flex-col items-center justify-center bg-black">
+	<h1 class=" text-7xl font-semibold text-white">Hello World</h1>
+</section>
