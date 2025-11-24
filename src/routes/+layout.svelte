8a1dd50f8420d15ff74bfaa5cc5f3773b0a8f4e9
zalwan <rizalsuryawan86@gmail.com>
2025-11-24 22:34:35 +0700

    feat: update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="https://fav.farm/⚡" />
 </svelte:head>
 
 {@render children()}
